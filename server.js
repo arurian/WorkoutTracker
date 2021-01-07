34bd71b3920ac5f735063d4234c5bbb541616496
arurian <arunvb2004@gmail.com>
2021-01-07 16:45:22 +1030

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://User-Arurian:Userarun@123@cluster0.vsieu.mongodb.net/workoutPlan?retryWrites=true&w=majority", { useNewUrlParser: true });
+
+const db = require("./models");
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
